b88db428f7e74d99c47c20cbf15e2dee9c1f5d37
Komol Kunty Rajib <rajib2@qmail.com.bd>
2023-12-22 17:11:03 +0600

    Update the algorithm for dead functions detection
        > Optimize the dead function detection code
        + (WIP) Dead variables detection

--- dead_codes/1.py
+++ dead_codes/1.py
@@ -1,9 +1,27 @@
 def add(a, b):
+    c = a
+    x = 56
+    y = 9
+    a = y
+
+    r = None
+
     return a + b
 
 
+ThisIs = "East West University"
+
+
 def subtract(a, b):
+    c = 89
+    print(c)
+
+    x = 56
+    y = 9
     return a - b
+
+
+AAA = 56
 
 
 def multiply(a, b):
@@ -17,3 +35,7 @@ def divide(a, b):
 divide(2, 1)
 
 subtract(7, 6)
+
+BBB = AAA
+
+print(BBB)
--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import re
 
 
-def detect_dead_code(filepath):
+def main(filepath):
+    code_mapping = {}
+    source_code_lines = []
+
     with open(filepath) as f:
-        source_code = f.read()
+        for line_number, line in enumerate(f, start=1):
+            code_mapping[line.strip()] = line_number
+            source_code_lines.append(line.strip())
 
-    looks_for_dead_functions(source_code=source_code)
+    source_code = '\n'.join(source_code_lines)
+
+    looks_for_dead_function(source_code, code_mapping)
+    looks_for_dead_variable(source_code, code_mapping)
 
 
-def looks_for_dead_functions(source_code):
-    function_define = re.findall(r'^def\s+([a-zA-Z_]\w*)\s*\(([^)]*)\)\s*:\s*$', source_code, re.MULTILINE)
-    
-    if function_define:
-        for match in function_define:
-            function_name = match[0]
+def looks_for_dead_variable(source_code, code_mapping):
+    variable_define_search = re.findall(r'^\s*([a-zA-Z_]\w*)\s*=\s*(.*?)(?=\n|\Z)', source_code, re.MULTILINE)
 
-            function_calls = re.findall(rf'^\b(?!def\s+)({function_name})(\((.*?)\))?$', source_code, re.MULTILINE)
+    print(variable_define_search)
 
-            if not function_calls:
-                print(f'Unused function found def {match[0]}({match[1]}).')
+
+def looks_for_dead_function(source_code, code_mapping):
+    function_define_search = re.findall(r'^def\s+([a-zA-Z_]\w*)\s*\(([^)]*)\)\s*:', source_code, re.MULTILINE)
+
+    print('Unused function found:')
+    for function_name, function_args in function_define_search:
+        function_calls = re.findall(rf'^\b(?!def\s+){function_name}\((.*?)\)?\s*$', source_code, re.MULTILINE)
+
+        if not function_calls:
+            function_full_define = f'def {function_name}({function_args}):'
+            print(f'{function_full_define} at line {code_mapping[function_full_define]}.')
 
 
 if __name__ == "__main__":
-    detect_dead_code("dead_codes/1.py")
+    main("dead_codes/1.py")
