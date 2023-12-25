942cc73009baff2570402c0642aeaa457ab95cd4
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2023-12-25 23:17:07 +0600

    removed comments

--- dead_codes/1.c
+++ dead_codes/1.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-// Function with dead code
 void unusedFunction() {
     printf("This function is never called.\n");
 }
 
-// Function with actual use
 void usedFunction() {
     printf("This function is called.\n");
 }
 
 
 int main() {
-    // Function call
     usedFunction();
 
     return 0;
--- main_for_c.py
+++ main_for_c.py
@@ -1,25 +1,20 @@
 import re
 
+
 def detect_dead_code(file_path):
     with open(file_path, 'r') as file:
         source_code = file.read()
 
-    # Define a regular expression for function declarations
     function_declaration_pattern = re.compile(r'\b(?:int|void|char|float)\s+([a-zA-Z_]\w*)\s*\([^)]*\)\s*{')
 
-    # Find all function declarations in the source code
     function_declarations = function_declaration_pattern.findall(source_code)
 
-    # Define a regular expression for function calls
     function_call_pattern = re.compile(r'\b([a-zA-Z_]\w*)\s*\([^)]*\)\s*;')
 
-    # Find all function calls in the source code
     function_calls = function_call_pattern.findall(source_code)
 
-    # Identify potential dead code by comparing function declarations and function calls
     dead_code = set(function_declarations) - set(function_calls)
 
-    # Print potential dead code
     for dead_function in dead_code:
         if dead_function == 'main':
             continue
@@ -27,5 +22,4 @@ def detect_dead_code(file_path):
 
 
 if __name__ == '__main__':
-    # Example usage:
     detect_dead_code('dead_codes/1.c')
